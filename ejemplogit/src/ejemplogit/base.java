1e9a9b911f262bada80de7a5191c4ac91c580f12
juanpablogarzon <juanpablogarzon@192.168.0.4>
2016-04-26 07:04:24 -0500

    ca,bio 2

--- ejemplogit/src/ejemplogit/base.java
+++ ejemplogit/src/ejemplogit/base.java
@@ -4,7 +4,7 @@ public class base {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola  jhjh hhjhj bbnmundo");
+		System.out.println("nada cambio 2");
 	}
 
 }
